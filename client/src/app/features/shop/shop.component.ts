ad3c9617bf58992fcfb378121de25261a0f4281e
Michał Bartczak <mbartczak159@gmail.com>
2025-11-17 22:09:15 +0100

    Fronted: searxh and some styling

--- client/src/app/features/shop/shop.component.ts
+++ client/src/app/features/shop/shop.component.ts
@@ -4,25 +4,28 @@ import { ShopService } from '../../core/services/shop';
 import { ProductItemComponent } from "./product-item/product-item.component";
 import { MatDialog } from "@angular/material/dialog"
 import { FiltersDialogComponent } from './filters-dialog/filters-dialog.component';
-import { MatButton } from '@angular/material/button';
+import { MatButton, MatIconButton } from '@angular/material/button';
 import { MatIcon } from '@angular/material/icon';
 import { MatListOption, MatSelectionList, MatSelectionListChange } from '@angular/material/list';
 import { MatMenu, MatMenuTrigger } from '@angular/material/menu';
 import { ShopParams } from '../../shared/models/shopParams';
 import { MatPaginator, PageEvent } from '@angular/material/paginator'
 import { Pagination } from '../../shared/models/pagination';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-shop',
   imports: [
-    ProductItemComponent, 
+    ProductItemComponent,
     MatButton,
     MatIcon,
     MatMenu,
     MatSelectionList,
     MatListOption,
     MatMenuTrigger,
-    MatPaginator
+    MatPaginator,
+    FormsModule,
+    MatIconButton
 ],
   templateUrl: './shop.component.html',
   styleUrl: './shop.component.scss',
@@ -55,6 +58,11 @@ export class ShopComponent implements OnInit {
       next: response => this.products = response,
       error: error => console.log(error)
     })
+  }
+
+  onSearchChange(){
+    this.shopParams.pageNumber = 1;
+    this.getProducts();
   }
 
   handlePageEvent(event: PageEvent){
